0ba067d2fc2158cd64ecab8b7ca1af2e12778b32
Bui Huy Hoang <hoangbh2709@gmail.com>
2023-11-12 11:26:31 +0700

    Update

---
+++ BaiTapOOP/src/VaoRaFile/J07072_ChuanHoaVaSapXep/Main.java
@@ -0,0 +1,21 @@
+package VaoRaFile.J07072_ChuanHoaVaSapXep;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws FileNotFoundException {
+        Scanner sc = new Scanner(new File("DANHSACH.in"));
+        ArrayList<HoTen> a = new ArrayList<>();
+        while (sc.hasNextLine()) {
+            a.add(new HoTen(sc.nextLine()));
+        }
+        Collections.sort(a);
+        for (HoTen i : a) {
+            System.out.println(i);
+        }
+    }
+}
